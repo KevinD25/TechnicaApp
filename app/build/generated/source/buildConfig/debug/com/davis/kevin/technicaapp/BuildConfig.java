8950f22c8b998ebb963abc4e528454fc534d8336
Rob <s100934@ap.be>
2020-10-12 21:02:36 +0200

    ?

---
+++ app/build/generated/source/buildConfig/debug/com/davis/kevin/technicaapp/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.davis.kevin.technicaapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.davis.kevin.technicaapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
