b8f46e5cda63b60f75cf744fc77e9d3344fe7544
Scazi <kevin-davis@hotmail.com>
2020-11-10 13:42:51 +0100

    Added firestore dependencies

--- API tool/src/app/app.module.ts
+++ API tool/src/app/app.module.ts
@@ -8,10 +8,17 @@ import { InfoComponent } from './info/info.component';
 import { EventsComponent } from './events/events.component';
 import { PartnersComponent } from './partners/partners.component';
 import { VacaturesComponent } from './vacatures/vacatures.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
-import {TokenInterceptorService} from './token-interceptor.service'
-import {FormsModule,ReactiveFormsModule} from '@angular/forms';
-import { NavBarComponent } from './nav-bar/nav-bar.component'
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {TokenInterceptorService} from './token-interceptor.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {AngularFireModule} from 'angularfire2';
+import {environment} from '../environments/environment';
+
+export const firebaseConfig = environment.firebaseConfig;
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import { FirestoreService } from './services/firestore.service';
+
 
 @NgModule({
   declarations: [
@@ -28,9 +35,13 @@ import { NavBarComponent } from './nav-bar/nav-bar.component'
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [
+    FirestoreService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
