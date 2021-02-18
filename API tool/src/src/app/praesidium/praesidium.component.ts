922d8bd222bd11bd0fb471bc49969a161d576360
Stef Martens <stef.m@hotmail.be>
2021-02-18 23:38:53 +0100

    DDL functies en foto upload gefixt

--- API tool/src/src/app/praesidium/praesidium.component.ts
+++ API tool/src/src/app/praesidium/praesidium.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DataserviceService } from "../Services/dataservice.service";
 
+import { AngularFireStorage } from "@angular/fire/storage";
+import { map, finalize } from "rxjs/operators";
+import { Observable } from "rxjs";
+
 @Component({
   selector: 'app-praesidium',
   templateUrl: './praesidium.component.html',
@@ -9,9 +13,15 @@ import { DataserviceService } from "../Services/dataservice.service";
 export class PraesidiumComponent implements OnInit {
 
 
-  constructor(public DataserviceService:DataserviceService) { }
+  constructor(public DataserviceService:DataserviceService, private storage: AngularFireStorage) { }
 
   PRAESIDIUM;
+  selectedFile: File = null;
+  n;
+  fb;
+  downloadURL: Observable<string>;
+  url : string;
+  file:File;
   ngOnInit(): void 
   {
     this.getPraesidium();
@@ -21,9 +31,14 @@ export class PraesidiumComponent implements OnInit {
 
   getPraesidium = () => this.DataserviceService.getPraesidium().subscribe(res => (this.PRAESIDIUM = res))
 
-  CREATE() 
+  async CREATE() 
   {
-    
+    this.onUpload();    
+    await this.delay(5000);
+
+    this.DataserviceService.PraesidiumForm.patchValue({
+      imageLink: `Praesidium/${this.n}`
+    });
 
     let data = this.DataserviceService.PraesidiumForm.value;
    this.DataserviceService.CreatePraesidiumLid(data)
@@ -43,5 +58,41 @@ export class PraesidiumComponent implements OnInit {
   {
 
   }
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
+onFileSelected(event) 
+  {
+    this.n = Date.now();
+    this.file = event.target.files[0];
+  }
+
+  onUpload()
+  {
+    const filePath = `Praesidium/${this.n}`;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(`Praesidium/${this.n}`, this.file);
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.downloadURL = fileRef.getDownloadURL();
+          this.downloadURL.subscribe(url => {
+            if (url) {
+              this.fb = url;
+              this.url = this.fb;
+            }
+
+          });
+        })
+      )
+      .subscribe(url => {
+        if (url) 
+        {
+          console.log(url);
+        }
+      });
+  
+    }
 }
