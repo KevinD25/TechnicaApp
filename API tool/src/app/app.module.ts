6722fdc1fc3ba949b03409b4f4c279fd23f8761e
Thomas Kramp <S115472@ap.be>
2021-07-21 17:56:44 +0200

    Organisatie

--- API tool/src/app/app.module.ts
+++ API tool/src/app/app.module.ts
@@ -1,38 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule,FormsModule } from "@angular/forms";
-import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { SponsorsComponent } from './sponsors/sponsors.component';
-import { VacaturesComponent } from './vacatures/vacatures.component';
-import { ClubTekstComponent } from './club-tekst/club-tekst.component';
-import { HomeComponent } from './home/home.component';
-import { PraesidiumComponent } from './praesidium/praesidium.component';
 import { environment } from "src/environments/environment";
 import { AngularFireModule } from "@angular/fire";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { EditorComponent } from './components/editor/editor.component';
-import { ViewerComponent } from './components/viewer/viewer.component';
+
 import { DataComponent } from './components/data/data.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ViewerComponent } from './components/viewer/viewer.component';
+
+import { ClubTekstComponent } from './club-tekst/club-tekst.component';
+import { HomeComponent } from './home/home.component';
+import { PraesidiumComponent } from './praesidium/praesidium.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
+import { VacaturesComponent } from './vacatures/vacatures.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SponsorsComponent,
-    VacaturesComponent,
-    ClubTekstComponent,
-    HomeComponent,
-    PraesidiumComponent,
     NavBarComponent,
     EditorComponent,
     ViewerComponent,
-    DataComponent
+    DataComponent,
+    
+    ClubTekstComponent,
+    HomeComponent,
+    PraesidiumComponent,
+    SponsorsComponent,
+    VacaturesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
